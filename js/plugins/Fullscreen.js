f406181556563f173f9095f411d7868a4ecc6933
icarobernard <icarobernar@hotmail.com>
2022-03-29 14:11:12 -0300

    initial

---
+++ js/plugins/Fullscreen.js
@@ -0,0 +1,25 @@
+(function() {
+  function extend(obj, name, func) {
+    var orig = obj.prototype[name]
+    obj.prototype[name] = function() {
+      orig.call(this)
+      func.call(this)
+    }
+  }
+ 
+  extend(Scene_Boot, 'start', function() {
+  Graphics._switchFullScreen();
+  })
+  
+  
+ var _Scene_Base_create = Scene_Base.prototype.create;
+
+ Scene_Base.prototype.create = function() {
+  _Scene_Base_create.call(this);
+  Graphics.width = 1280;
+  Graphics.height = 720; 
+  Graphics.boxHeight = 720;
+  Graphics.boxWidth = 1280; 
+ };
+ 
+})()
